fd57aef3c57ef511350b9c8b10444115f8a6eac3
Farhat <farhat00706@gmail.com>
2025-12-19 20:09:20 +0500

    Первый коммит

---
+++ main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('8215602522:AAGAfq0jnJ_9cLbryWWbeF9LAbj_GGOfjaE')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://itproger.com')))
+    await message.answer('Привет, мой друг!', reply_markup=markup)
+
+executor.start_polling(dp)
